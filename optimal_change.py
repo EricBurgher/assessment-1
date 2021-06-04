742f51f10f6a4c5b308aa44b342303dbab60a5e7
Eric Burgher <ericburgher@Erics-Air.attlocal.net>
2021-06-04 13:00:00 -0500

    First go save point

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,16 @@
 # Write your solution here!
+# 
+def optimal_change(item_cost, amount_paid):
+
+    denomination = [100, 50, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
+    denomination_names = ['$100 bill', '$50 bill', '$20 bill', '$10 bill', '$5 bill', '$1 bill', 'quarter', 'dime', 'nickel', 'penny']
+
+    change = 0
+    for x in denomination:
+        while item_cost >= denomination:
+            item_cost = item_cost - denomination
+            change = change + 1
+
+    optimal_change(66.66, 80)
+
+    print("The optimal change for an item that costs " + (item_cost) + "with an amount paid of " + (amount_paid) + "is " + (change))
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,4 @@
 # Write your unit tests here!
+from assessment-1 import optimal_change
+
+if __name__ == "__main__":
