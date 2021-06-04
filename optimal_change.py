7780fc4a06f45e84c2c76a5cb1fb2d71fd41be47
Eric Burgher <ericburgher@Erics-Air.attlocal.net>
2021-06-04 15:04:21 -0500

    Third save point

--- optimal_change.py
+++ optimal_change.py
@@ -5,10 +5,14 @@ def optimal_change(item_cost, amount_paid):
     denomination = [100, 50, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
     denomination_names = ['$100 bill', '$50 bill', '$20 bill', '$10 bill', '$5 bill', '$1 bill', 'quarter', 'dime', 'nickel', 'penny']
 
+    class vending_machine:
+        def __init__(self):
+
     change = 0
     for x in denomination:
         while amount_paid >= item_cost:
             change = amount_paid - item_cost
+
 
     optimal_change(66.66, 80)
 
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,10 +1,11 @@
 # Write your unit tests here!
 import unittest
-from assessment-1 import optimal_change
+from optimal_change import optimal_change
 
 class OptimalChangeTestCase(unittest.testcase)
 """Tests for the code"""
-    def test_change_return(self):
+    def test_change_return_string(self):
+        self.assertTrue(type(vending_machine.optimal_change(66.66, 80)), str)
 
 if __name__ == "__main__":
     unittest.main()
