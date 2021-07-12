c76b46686972715d5782264a7b2beca134314361
Eric Burgher <ericburgher@Erics-Air-2.attlocal.net>
2021-07-12 05:31:28 -0500

    Assessment 1

--- optimal_change.py
+++ optimal_change.py
@@ -1,21 +1,43 @@
 # Write your solution here!
 # 
-class vending_machine:
-    def __init__(self):
-
+import math
 
 def optimal_change(item_cost, amount_paid):
 
-    denomination = [100, 50, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
-    denomination_names = ['$100 bill', '$50 bill', '$20 bill', '$10 bill', '$5 bill', '$1 bill', 'quarter', 'dime', 'nickel', 'penny']
+    denomination = [100, 50, 20, 10, 5, 1, .25, .1, .05, .01]
 
+    change = float(amount_paid - item_cost)
 
-    change = 0
-    for x in denomination:
-        while amount_paid >= item_cost:
-            change = amount_paid - item_cost
+    output = f'The optimal change for this item is ${item_cost} with an amount paid of {amount_paid} is '
 
+    if amount_paid == item_cost:
+        return ("No Change.")
 
-    optimal_change(66.66, 80)
+    if amount_paid < item_cost:
+        return("You dont have enough money for this purchase.")
 
-    print("The optimal change for an item that costs " + (item_cost) + "with an amount paid of " + (amount_paid) + "is " + (change))
+    if change > 0:
+        for x in range(len(denomination)):
+            amount = math.floor(change // denomination[x])
+
+            if amount > 1:
+                if x >= 6 and amount > 0:
+                    output += f' , {amount} ${denomination[x]} cents'
+                    change -= (amount * denomination[x])
+                    continue
+                elif amount > 0:
+                    output += f', {amount} ${denomination[x]} bills'
+                    change -=(amount * denomination[x])
+
+            elif amount > 0:
+                if x >= 6 and amount > 0:
+                    output += f' {amount} ${denomination[x]} cent'
+                    change -= (amount * denomination[x])
+                    continue
+                elif amount > 0:
+                    output += f' {amount} ${denomination[x]} bill'
+                    change -= (amount * denomination[x])
+
+        return output + '.'
+
+    print(optimal_change(66.66, 80))
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -2,10 +2,16 @@
 import unittest
 from optimal_change import optimal_change
 
-class OptimalChangeTestCase(unittest.testcase)
-"""Tests for the code"""
+class OptimalChangeTestCase(unittest.TestCase):
+    """Tests for the code"""
     def test_change_return_string(self):
-        self.assertTrue(type(vending_machine.optimal_change(66.66, 80)), str)
+        self.assertTrue(optimal_change(66.66, 80), 'The optimal change for this item is 13.13 with an amount paid of $80 is 1 $10 bill, 3 $1 bills, 1 dime and 3 pennies.' )
+
+    def test_change_2(self):
+        self.assertTrue(optimal_change(20, 20)) == "No Change."
+
+    def test_change_3(self):
+        self.assertTrue(optimal_change(100, 80)), "You dont have enough money for this purchase."
 
 if __name__ == "__main__":
     unittest.main()
